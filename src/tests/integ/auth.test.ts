42cf4e5286c089503c583f5b5cc73733a6cb5864
Matthieu Marin <matthieumarin51@gmail.com>
2025-03-24 14:48:50 +0100

    Feature: [AIC-008] -  sign up owner
    * feat: add prisma model for bdd
    
    * test: add unit test and integ test
    
    * fix: Remove .idea

---
+++ src/tests/integ/auth.test.ts
@@ -0,0 +1,48 @@
+import request from 'supertest';
+import app from '../../index';
+
+describe('Authentication route tests.', () => {
+
+    const dataTest = [
+        {input: { email: 'testapprouved'+Math.floor(Math.random() * 1000000)+'@example.com', password: 'ValidPass1!', lname: "pedro", tel: "123456789", fname: "toto", status: "owner"}, expected: { message: 'User created successfully', responseCode : 201, messagePath: "message"} , itTitle: 'Should register a new owner.'}, // NOSONAR
+        {input: { email: 'testbadpassword'+Math.floor(Math.random() * 1000000)+'@example.com', password: 'short', lname: "pedro", tel: "123456789", fname: "toto", status: "owner" }, expected: { message: 'The password must be at least 8 characters long.', responseCode : 400, messagePath: "error"}, itTitle: 'Should reject a registration with a short password.'}, // NOSONAR
+        {input: { email: 'testunknownStatus'+Math.floor(Math.random() * 1000000)+'@example.com', password: 'ValidPass1!', lname: "pedro", tel: "123456789", fname: "toto", status: "Spaghetti" }, expected: { message: 'Error during registration :unknown status', responseCode : 400, messagePath: "error"}, itTitle: 'Should reject a registration with an unknown status.'}, // NOSONAR
+        {input: { email: 'testbadpassword'+Math.floor(Math.random() * 1000000)+'@example.com', password: 'short', lname: "pedro", fname: "toto", status: "owner" }, expected: { message: 'Error during registration : missing information', responseCode : 400, messagePath: "error"}, itTitle: 'Should reject a registration with a missing information.'}, // NOSONAR
+    ]
+
+  for (const data of dataTest) {
+        it(data.itTitle, async () => {
+            const res = await request(app)
+                .post('/auth/signup')
+                .send(data.input);
+            expect(res.statusCode).toEqual(data.expected.responseCode);
+            expect(res.body).toHaveProperty(data.expected.messagePath, data.expected.message);
+        });
+    }
+
+    it('Should reject a registration with an already exist email.', async () => {
+        const originalRes = await request(app)
+            .post('/auth/signup')
+            .send({
+                email: 'existingemail@example.com',
+                password: 'ValidPass1!', // NOSONAR
+                lname: "pedro",
+                tel: "123456789",
+                fname: "toto",
+                status: "owner"
+            });
+
+        const res = await request(app)
+            .post('/auth/signup')
+            .send({
+                email: 'existingemail@example.com',
+                password: 'ValidPass1!', // NOSONAR
+                lname: "pedro",
+                tel: "123456789",
+                fname: "toto",
+                status: "owner"
+            });
+        expect(res.statusCode).toEqual(400);
+        expect(res.body).toHaveProperty('error', 'Error during registration :Email already exists');
+    });
+});
