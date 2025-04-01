03fbc35affd32533b6a78ad6a4d25c72fbd26500
Matthieu Marin <matthieumarin51@gmail.com>
2025-04-01 10:14:56 +0200

    fix: remove a usefull console log... ughhh

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ app.disable("x-powered-by");
 
 if (process.env.NODE_ENV !== "test") {
   const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  app.listen(PORT);
 }
 
 export default app;
