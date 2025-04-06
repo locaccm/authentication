3069c08cbcad3635a78b201c9b80d2a7ee7dc5f8
Matthieu <matthieumarin51@gmail.com>
2025-04-06 14:05:43 +0200

    Feat : AIC-106 update authentication with new db

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -4,88 +4,37 @@ import User from "../models/user";
 
 const prisma = new PrismaClient();
 
-export const registerOwner = async (user: User) => {
-  if (
-    !user.lname ||
-    !user.fname ||
-    !user.tel ||
-    !user.email ||
-    !user.password ||
-    !user.status
-  ) {
+export const registerUser = async (user: User) => {
+  if (!user.hasAllAttributesForRegister()) {
     throw new Error("missing information");
   }
-  const hashedPassword = await bcrypt.hash(user.password, 10);
+  user.USEC_PASSWORD = await bcrypt.hash(user.USEC_PASSWORD!, 10);
 
-  return prisma.owner.create({
+  const userInDb = await prisma.user.create({
     data: {
-      OWNC_FNAME: user.fname,
-      OWNC_LNAME: user.lname,
-      OWNN_TEL: user.tel,
-      OWNC_MAIL: user.email,
-      OWNC_MDP: hashedPassword,
+      ...user
     },
   });
+
+  return new User(user.USEC_MAIL).mapDbUserToModel(userInDb);
 };
 
-export const connectOwner = async (user: User) => {
-  const owner = await prisma.owner.findFirst({
+export const connectUser = async (user: User) => {
+  const userDb = await prisma.user.findFirst({
     where: {
-      OWNC_MAIL: user.email,
+      USEC_MAIL: user.USEC_MAIL,
     },
   });
-  if (!owner) {
+  if (!userDb) {
     return null;
   }
-  return owner;
-};
-export const connectTenant = async (user: User) => {
-  const tenant = await prisma.tenant.findFirst({
-    where: {
-      TENC_MAIL: user.email,
-    },
-  });
-  if (!tenant) {
-    return null;
-  }
-  return tenant;
+  return new User(user.USEC_MAIL).mapDbUserToModel(userDb);
 };
 
-export const emailOwnerExists = async (email: string) => {
-  return prisma.owner.findFirst({
+export const emailUserExists = async (email: string) => {
+  return !!prisma.user.findFirst({
     where: {
-      OWNC_MAIL: email,
-    },
-  });
-};
-export const registerTenant = async (user: User) => {
-  if (
-    !user.lname ||
-    !user.fname ||
-    !user.tel ||
-    !user.email ||
-    !user.password ||
-    !user.status
-  ) {
-    throw new Error("missing information");
-  }
-  const hashedPassword = await bcrypt.hash(user.password, 10);
-
-  return prisma.tenant.create({
-    data: {
-      TENC_FNAME: user.fname,
-      TENC_LNAME: user.lname,
-      TENN_TEL: user.tel,
-      TENC_MAIL: user.email,
-      TENC_MDP: hashedPassword,
-    },
-  });
-};
-
-export const emailTenantExists = async (email: string) => {
-  return prisma.tenant.findFirst({
-    where: {
-      TENC_MAIL: email,
+      USEC_MAIL: email,
     },
   });
 };
