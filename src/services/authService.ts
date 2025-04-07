877d26a49d47751024084546b96b35977d61b882
Matthieu Marin <matthieumarin51@gmail.com>
2025-04-07 16:20:47 +0200

    Feature: [AIC-063] - add dockerfile (#19)
    * Feature: [AIC-063] - add dockerfile
    
    * feat: aic 63 add dockerfile
    
    * fix: migration prisma
    
    * fix: gitignore

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import User from "../models/user";
 
 const prisma = new PrismaClient();
