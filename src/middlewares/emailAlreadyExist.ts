f0732d3a13ada04c6ad1fdfb80acfc1a2c35bfef
Matthieu <matthieumarin51@gmail.com>
2025-04-08 18:59:36 +0200

    Feat : AIC-106 update test authentication with new db

--- src/middlewares/emailAlreadyExist.ts
+++ src/middlewares/emailAlreadyExist.ts
@@ -3,7 +3,7 @@ import { Response } from "express";
 import { emailUserExists } from "../services/authService";
 
 export const emailAlreadyExist = async (email: string, res: Response) => {
-  if (!!(await emailUserExists(email))) {
+  if (await emailUserExists(email)) {
     throw new Error("Email already exists");
   }
 };
