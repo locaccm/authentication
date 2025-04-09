76c84a9a64cbc44163f66868cce34ab92cf5a904
Matthieu Marin <matthieumarin51@gmail.com>
2025-04-09 19:59:57 +0200

    :wq Feature: [AIC-063] - add dockerfile (#19) (#25)
    * Feature: [AIC-063] - add dockerfile
    
    * feat: aic 63 add dockerfile
    
    * fix: migration prisma
    
    * fix: gitignore

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -14,6 +14,7 @@ import emailInformation from "../config/emailInformation";
 
 const tokenDuration = 1000 * 60 * 60;
 
+
 export const signUp = async (req: Request, res: Response): Promise<void> => {
   try {
     const {
---
+++ src/map/mapDbOwnerToModel.ts
@@ -0,0 +1,19 @@
+import User from "../models/user";
+import { owner } from "@prisma/client";
+
+export const mapDbOwnerToModel = (dbOwner: null | owner): User | null => {
+  if (!dbOwner) {
+    return dbOwner;
+  }
+
+  const user: User = {
+    id: dbOwner.OWNN_ID,
+    lname: dbOwner.OWNC_LNAME,
+    fname: dbOwner.OWNC_FNAME,
+    tel: dbOwner.OWNN_TEL,
+    email: dbOwner.OWNC_MAIL,
+    password: dbOwner.OWNC_MDP ?? "",
+    status: "owner",
+  };
+  return user;
+};
