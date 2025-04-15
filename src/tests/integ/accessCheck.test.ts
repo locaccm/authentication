97da7045fd9f73a81e6ad4252bc8d1b7e316e1c9
Matthieu <matthieumarin51@gmail.com>
2025-04-15 19:15:15 +0200

    fix: put JWT_SECRET because i'm an idiot

--- src/tests/integ/accessCheck.test.ts
+++ src/tests/integ/accessCheck.test.ts
@@ -33,24 +33,12 @@ describe("access check", () => {
 
     for (const user of users) {
       const signupRes = await request(app).post("/auth/signup").send(user);
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 1a316d3 (fix: verification signin and signup)
       expect(signupRes.body).toHaveProperty("message", "User created successfully");
       expect(signupRes.status).toBe(201);
 
       const signinRes = await request(app).post("/auth/signin").send(user);
       expect(signinRes.body).toHaveProperty("message", "User connected successfully");
-<<<<<<< HEAD
-=======
       expect(signupRes.status).toBe(201);
-
-      const signinRes = await request(app).post("/auth/signin").send(user);
->>>>>>> b2056da (Feat: aic-54 token with test)
-=======
->>>>>>> 1a316d3 (fix: verification signin and signup)
-      expect(signinRes.status).toBe(200);
 
       const userType = user.getType();
       if (userType) {
@@ -59,31 +47,18 @@ describe("access check", () => {
         console.error("User  type is undefined for user:", user);
       }
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
   });
-=======
-  })
->>>>>>> b2056da (Feat: aic-54 token with test)
-=======
-  });
->>>>>>> 4e51096 (chore: prettier)
 
   describe("All access check tests.", () => {
     Object.entries(tokens).forEach(([roleName]) => {
       describe(`${roleName}`, () => {
         for (const [role, permissions] of Object.entries(rolesPermissions)) {
           describe(`Test with right of ${role}`, () => {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 4e51096 (chore: prettier)
             testByAllFunction(
               roleName,
               permissions,
               roleName != role && role != "everyone",
             );
-<<<<<<< HEAD
           });
         }
       });
@@ -114,36 +89,7 @@ function testByAllFunction(
   for (const rightName of rights) {
     it(`${rightName}`, async () => {
       const token = tokens[roleName.toString()];
-      const res = await request(app)
-        .post("/access/check")
-        .send({ token, rightName });
-=======
-            testByAllFunction(roleName, permissions, (roleName != role) && (role != "everyone"));
-=======
->>>>>>> 4e51096 (chore: prettier)
-          });
-        }
-      });
-    });
-  });
-});
-
-function testByAllFunction(
-  roleName: String,
-  rights: String[],
-  shouldFail: boolean,
-) {
-  for (const rightName of rights) {
-    it(`${rightName}`, async () => {
-      const token = tokens[roleName.toString()];
-<<<<<<< HEAD
       const res = await request(app).post("/access/check").send({ token, rightName });
->>>>>>> b2056da (Feat: aic-54 token with test)
-=======
-      const res = await request(app)
-        .post("/access/check")
-        .send({ token, rightName });
->>>>>>> 4e51096 (chore: prettier)
       if (shouldFail) {
         expect(res.statusCode).toEqual(403);
         expect(res.body).toHaveProperty("message", "Access denied");
@@ -153,12 +99,4 @@ function testByAllFunction(
       expect(res.body).toHaveProperty("message", "Access granted");
     });
   }
-<<<<<<< HEAD
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> b2056da (Feat: aic-54 token with test)
-=======
-}
->>>>>>> 4e51096 (chore: prettier)
