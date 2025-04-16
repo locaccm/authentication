eed60524701c27f14e5a8fe230281f5cb35cfe4c
matthieu.marin <matthieu.marin@renault.com>
2025-04-16 16:53:26 +0200

    feat/aic 09 invitation tenant

--- src/middlewares/emailAlreadyExist.ts
+++
@@ -1,9 +0,0 @@
-import { Response } from "express";
-
-import { emailUserExists } from "../services/authService";
-
-export const emailAlreadyExist = async (email: string, res: Response) => {
-  if (await emailUserExists(email)) {
-    throw new Error("Email already exists");
-  }
-};
