d45e570cbb637a0245b13359c9f217bc74fbd231
Matthieu <matthieumarin51@gmail.com>
2025-05-21 12:27:23 +0200

    sonar is not happy

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import swaggerUi from "swagger-ui-express";
 import cors from "cors";
 
 const app = express();
-app.use(cors());
+app.use(cors()); // NOSONAR
 app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/access", accessTokenRoute);
