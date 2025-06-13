1df78c779fd6ebbb4f7c080f672e32ffdbcca63a
Matthieu <matthieumarin51@gmail.com>
2025-06-14 01:27:07 +0200

    update cors

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,13 @@ import swaggerUi from "swagger-ui-express";
 import cors from "cors";
 
 const app = express();
-app.use(cors()); // NOSONAR
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN || "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  }),
+); // NOSONAR
 app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/access", accessTokenRoute);
