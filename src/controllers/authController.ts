5c7267f5fba5d08b68eac5e750756c0fba41d45c
Matthieu <matthieumarin51@gmail.com>
2025-06-15 16:48:15 +0200

    fix owner

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -50,6 +50,8 @@ export const signUp = async (req: Request, res: Response): Promise<void> => {
         });
         return;
       }
+    } else {
+      user.setStatus("OWNER");
     }
     userInDb = await registerUser(user);
     userInDb.removePassword();
